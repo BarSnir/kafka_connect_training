62386b296f659f5521d9c210daad197094b72700
Bar Snir <bar1991snir@gmail.com>
2024-02-19 13:07:17 +0200

    done with connector?

--- free-sounds-connect/src/main/java/com/connect/app/SoundsSourceConnectorConfig.java
+++ free-sounds-connect/src/main/java/com/connect/app/SoundsSourceConnectorConfig.java
@@ -22,6 +22,9 @@ public class SoundsSourceConnectorConfig extends AbstractConfig {
     private static final String TERMS_LIST_DOC = "Terms string list to search in free-source.org";
     public static final String INTERVAL_CONFIG = "interval";
     private static final String INTERVAL_DOC = "Time to wait between to api requests, in milliseconds..";
+    public static final String MONITOR_THREAD_TIMEOUT_CONFIG = "monitor.thread.timeout";
+    private static final String MONITOR_THREAD_TIMEOUT_DOC = "Timeout used by the monitoring thread";
+    private static final int MONITOR_THREAD_TIMEOUT_DEFAULT = 10000;
     public static final ConfigDef CONFIG_DEF = createConfigDef();
 
     private static ConfigDef createConfigDef() {
@@ -52,6 +55,12 @@ public class SoundsSourceConnectorConfig extends AbstractConfig {
             5000,
             Importance.MEDIUM,
             INTERVAL_DOC
+        ).define(
+            MONITOR_THREAD_TIMEOUT_CONFIG,
+            Type.INT,
+            MONITOR_THREAD_TIMEOUT_DEFAULT,
+            Importance.LOW,
+            MONITOR_THREAD_TIMEOUT_DOC
         );
     }
 }
--- free-sounds-connect/src/main/java/com/connect/app/SourceSourceConnector.java
+++ free-sounds-connect/src/main/java/com/connect/app/SourceSourceConnector.java
@@ -9,11 +9,8 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.kafka.common.config.Config;
 import org.apache.kafka.common.config.ConfigDef;
-import org.apache.kafka.common.config.ConfigValue;
 import org.apache.kafka.connect.connector.Task;
-import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.source.SourceConnector;
 import org.apache.kafka.connect.util.ConnectorUtils;
 
@@ -27,12 +24,53 @@ public class SourceSourceConnector extends SourceConnector{
     private NewPartitionsCheckerThread checker;
 
     @Override
+    public String version() {
+        return PropertiesUtil.getConnectorVersion();
+    }
+
+    @Override
     public ConfigDef config() {
         return CONFIG_DEF;
     }
 
     @Override
     public Class<? extends Task> taskClass() {
-        return MyFirstKafkaConnectorTask.class;
+        return SoundsSourceConnectorTask.class;
+    }
+
+    @Override
+    public void start(Map<String, String> originalProps) {
+        this.originalProps = originalProps;
+        config = new SoundsSourceConnectorConfig(originalProps);
+        int monitorThreadTimeout = config.getInt(MONITOR_THREAD_TIMEOUT_CONFIG);
+        checker = new NewPartitionsCheckerThread(context, monitorThreadTimeout);
+        checker.start();
+    }
+
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        List<Map<String, String>> taskConfigs = new ArrayList<>();
+        // The partitions below represent the source's part that
+        // would likely to be broken down into tasks... such as
+        // tables in a database.
+        List<String> partitions = checker.getCurrentSources();
+        if (partitions.isEmpty()) {
+            taskConfigs = Collections.emptyList();
+            log.warn("No tasks created because there is zero to work on");
+        } else {
+            int numTasks = Math.min(partitions.size(), maxTasks);
+            List<List<String>> partitionSources = ConnectorUtils.groupPartitions(partitions, numTasks);
+            for (List<String> source : partitionSources) {
+                Map<String, String> taskConfig = new HashMap<>(originalProps);
+                taskConfig.put("sources", String.join(",", source));
+                taskConfigs.add(taskConfig);
+            }
+        }
+        return taskConfigs;
+    }
+
+    @Override
+    public void stop() {
+        checker.shutdown();
     }
 }
