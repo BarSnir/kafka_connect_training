dbf624ac81e8f4d4e2c6e56c78d045f6ff3ce990
Bar Snir <bar1991snir@gmail.com>
2024-02-19 10:22:58 +0200

    added Connector config class

---
+++ free-sounds-connect/src/main/java/com/connect/app/SoundsSourceConnectorConfig.java
@@ -0,0 +1,57 @@
+package com.connect.app;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import org.apache.kafka.common.config.ConfigDef.Type;
+
+public class SoundsSourceConnectorConfig extends AbstractConfig {
+
+    public SoundsSourceConnectorConfig(final Map<?, ?> originalProps) {
+        super(CONFIG_DEF, originalProps);
+        
+    }
+
+    public static final String TOPIC_CONFIG = "topic";
+    private static final String TOPIC_DOC = "Topic to publish";
+    public static final String API_KEY_CONFIG = "api.key";
+    private static final String API_KEY_DOC = "Api key of free-source.org";
+    public static final String TERMS_LIST_CONFIG = "terms.list";
+    private static final String TERMS_LIST_DOC = "Terms string list to search in free-source.org";
+    public static final String INTERVAL_CONFIG = "interval";
+    private static final String INTERVAL_DOC = "Time to wait between to api requests, in milliseconds..";
+    public static final ConfigDef CONFIG_DEF = createConfigDef();
+
+    private static ConfigDef createConfigDef() {
+        ConfigDef configDef = new ConfigDef();
+        addParams(configDef);
+        return configDef;
+    }
+
+    private static void addParams(final ConfigDef configDef) {
+        configDef.define(
+            TOPIC_CONFIG,
+            Type.STRING,
+            Importance.HIGH,
+            TOPIC_DOC)
+        .define(
+            API_KEY_CONFIG,
+            Type.STRING,
+            Importance.HIGH,
+            API_KEY_DOC)
+        .define(
+            TERMS_LIST_CONFIG,
+            Type.STRING,
+            Importance.HIGH,
+            TERMS_LIST_DOC)
+        .define(
+            INTERVAL_CONFIG,
+            Type.INT,
+            5000,
+            Importance.MEDIUM,
+            INTERVAL_DOC
+        );
+    }
+}
